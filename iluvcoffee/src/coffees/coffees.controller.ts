8309994deaf210a852d315b9e8a01d0c41139688
Manuel Spuhler <manuel.spuhler@gmail.com>
2022-02-25 10:29:18 +0100

    Coffee service

--- iluvcoffee/src/coffees/coffees.controller.ts
+++ iluvcoffee/src/coffees/coffees.controller.ts
@@ -1,22 +1,33 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+    constructor(private readonly coffeesService: CoffeesService) { }
 
     @Get()
-    findAll() {
-        return 'all coffees'
+    findAll(@Query() paginationQuery) {
+        // const { limit, offset } = paginationQuery;
+        return this.coffeesService.findAll();
     }
 
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return `${id} coffee`
+        return this.coffeesService.findOne(id);
     }
 
     @Post()
-    @HttpCode(HttpStatus.GONE)
     create(@Body() body) {
-        return body
+        return this.coffeesService.create(body);
     }
 
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body) {
+        return this.coffeesService.update(id, body);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.coffeesService.remove(id);
+    }
 }
