987cba1a7a58735ebc668684fbd20e5a1407b3bf
Manuel Spuhler <manuel.spuhler@gmail.com>
2022-03-07 14:14:38 +0100

    fetching relations

--- iluvcoffee/src/coffees/coffees.service.ts
+++ iluvcoffee/src/coffees/coffees.service.ts
@@ -12,19 +12,23 @@ export class CoffeesService {
     ) { }
 
     findAll() {
-        return this.coffeeRepository.find();
+        return this.coffeeRepository.find({ 
+            relations: ['flavors'] 
+        });
     }
 
     async findOne(id: number) {
-        const coffee = await this.coffeeRepository.findOne(id)
+        const coffee = await this.coffeeRepository.findOne(id, { 
+            relations: ['flavors'] 
+        })
         if (!coffee) throw new NotFoundException(`Coffe #${id} not found`)
-        
+
         return coffee
     }
 
     async create(createCoffeeDto: CreateCoffeeDto) {
         const coffee = this.coffeeRepository.create(createCoffeeDto)
-        
+
         return this.coffeeRepository.save(coffee)
     }
 
