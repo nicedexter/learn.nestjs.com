c32cb378cc1e8886fb5be43c402932f09327e59a
Manuel Spuhler <manuel.spuhler@gmail.com>
2022-03-22 10:11:50 +0100

    Joi

--- iluvcoffee/src/app.module.ts
+++ iluvcoffee/src/app.module.ts
@@ -1,14 +1,20 @@
+import * as Joi from '@hapi/joi'
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module'
 import { CoffeesModule } from './coffees/coffees.module'
-import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
-import { ConfigModule } from '@nestjs/config'
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432)
+      })
+    }),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
