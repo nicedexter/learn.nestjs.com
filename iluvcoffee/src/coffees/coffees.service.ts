1a200b2b0f7a1bd6ff664c8bc132f4921a48ab73
Manuel Spuhler <manuel.spuhler@gmail.com>
2022-03-22 10:37:49 +0100

    forRootAsync

--- iluvcoffee/src/coffees/coffees.service.ts
+++ iluvcoffee/src/coffees/coffees.service.ts
@@ -1,14 +1,13 @@
 import { Inject, Injectable, NotFoundException } from '@nestjs/common'
+import { ConfigType } from '@nestjs/config'
 import { InjectRepository } from '@nestjs/typeorm'
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto'
 import { Connection, Repository } from 'typeorm'
+import { Event } from '../events/entities/event.entity'
+import coffeesConfig from './config/coffees.config'
 import { CreateCoffeeDto } from './dto/create-coffee.dto'
 import { Coffee } from './entities/coffee.entity'
 import { Flavor } from './entities/flavor.entity'
-import { Event } from '../events/entities/event.entity'
-import { COFFEE_BRANDS } from './coffees.constants'
-import { ConfigService, ConfigType } from '@nestjs/config'
-import coffeesConfig from './config/coffees.config'
 
 @Injectable()
 export class CoffeesService {
