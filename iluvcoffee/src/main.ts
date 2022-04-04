469762d0237bb3cbaf2bd0e89dd7cf8bbbc829b4
Manuel Spuhler <manuel.spuhler@gmail.com>
2022-04-04 17:57:44 +0200

    Interceptor timeout

--- iluvcoffee/src/main.ts
+++ iluvcoffee/src/main.ts
@@ -3,11 +3,12 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { HttpExceptionFilter } from './common/filters/http-exception.filter'
 import { ApiKeyGuard } from './common/guards/api-key.guard'
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor'
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.useGlobalPipes(new ValidationPipe({  
+  app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
     forbidNonWhitelisted: true,
@@ -16,7 +17,7 @@ async function bootstrap() {
     }
   }))
   app.useGlobalFilters(new HttpExceptionFilter())
-  app.useGlobalInterceptors(new WrapResponseInterceptor())
+  app.useGlobalInterceptors(new WrapResponseInterceptor(), new TimeoutInterceptor())
   await app.listen(3000)
 }
 bootstrap()
